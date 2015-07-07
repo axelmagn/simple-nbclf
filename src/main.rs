8d45a4b541ec75029e42344f5f3983ac75ba48bb
Axel Magnuson <axelmagn@gmail.com>
2015-07-07 12:23:42 -0600

    Write data loading function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io::prelude::*;
+use std::io;
+use std::fs::File;
+use std::env;
+
+fn load_data(file_path : String) -> io::Result<String> {
+    let mut file = try!(File::open(file_path));
+    let mut s = String::new();
+    try!(file.read_to_string(&mut s));
+    Ok(s)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let argv: Vec<String> = env::args().collect();
+    let X_file_path = argv[1].clone();
+    let y_file_path = argv[2].clone();
+    println!("X: {}", X_file_path);
+    println!("Y: {}", y_file_path);
 }

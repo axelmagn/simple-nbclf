8c7f6446475d61ade7aac129ba6be64c06ab52f8
Axel Magnuson <axelmagn@gmail.com>
2015-07-08 14:10:23 -0600

    more work implementing NBClf

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+extern crate nalgebra as na;
+extern crate num;
+
 use std::io::prelude::*;
 use std::io;
 use std::fs::File;
@@ -5,13 +8,16 @@ use std::path::Path;
 use std::str::FromStr;
 use std::string::ToString;
 use std::env;
+use num::traits::Zero;
+use na::{DMat, DVec};
 
 struct DataMatrix<T> {
     data: Vec<T>,
     shape: (usize, usize),
 }
 
-impl<T: FromStr + ToString> DataMatrix<T> {
+
+impl<T: FromStr + ToString + Zero + Clone + Copy> DataMatrix<T> {
     fn open<P: AsRef<Path>>(file_path : P) -> io::Result<DataMatrix<T>> {
         let mut data: Vec<T> = Vec::new();
         let mut rows = 0;
@@ -71,35 +77,105 @@ impl<T: FromStr + ToString> DataMatrix<T> {
         let (_, cols) = self.shape;
         self.data[row * cols + col] = val;
     }
+
+    fn as_dmat(&self) -> DMat<T> {
+        let (rows, cols) = self.shape;
+        let mut out = DMat::new_zeros(rows, cols);
+        for i in 0..rows {
+            for j in 0..cols {
+                out[(i,j)] = self.data[i*cols + j];
+            }
+        }
+        out
+    }
 }
 
 
 struct MultinomialNB {
-    alpha: f32,
-    fit_prior: bool,
+    // alpha: f32,
+    // fit_prior: bool,
     // shape: n_classes
-    class_log_prior: Vec<f64>,
-    class_count: Vec<usize>,
+    class_log_prior: DVec<f64>,
+    class_count: DVec<usize>,
     // shape: (n_classes, n_features) 
-    feature_log_prob: Vec<Vec<f64>>,
-    feature_count: Vec<Vec<usize>>
+    feature_log_prob: DMat<f64>,
+    feature_count: DMat<usize>,
 }
 
 
 impl MultinomialNB {
-    fn new(alpha: f32, fit_prior: bool) -> MultinomialNB {
-        MultinomialNB {
-            alpha: alpha,
-            fit_prior: fit_prior,
-            class_log_prior: Vec::new(),
-            class_count: Vec::new(),
-            feature_log_prob: Vec::new(),
-            feature_count: Vec::new(),
+    fn fit(x: DMat<usize>, y: DMat<usize>) -> Result<MultinomialNB, &'static str> {
+        let n_records = x.nrows();
+        let n_feats = x.ncols();
+        let n_classes = y.ncols();
+        if y.nrows() != n_records {
+            return Err("Data matrices have mismatched record count");
         }
+        let mut clf = MultinomialNB {
+            // alpha: alpha,
+            // fit_prior: fit_prior,
+            class_log_prior: DVec::new_zeros(n_classes),
+            class_count: DVec::new_zeros(n_classes),
+            feature_log_prob: DMat::new_zeros(n_classes, n_feats),
+            feature_count: DMat::new_zeros(n_classes, n_feats),
+        };
+        // Accumulate Counts
+        let mut n_observations = 0usize;
+        for i in 0..n_records {
+            for j in 0..n_classes {
+                if y[(i,j)] == 1 {
+                    for k in 0..n_feats {
+                        let n = x[(i,k)];
+                        n_observations += n;
+                        clf.class_count[j] += n;
+                        clf.feature_count[(j,k)] += n;
+                    }
+                } else if y[(i,j)] != 0 {
+                    return Err("Non-binary value found in y");
+                }
+            }
+        }
+        // compute log probabilities
+        let log_n_observations = (n_observations as f64).log2();
+        for c in 0..n_classes {
+            // pr(c) = class_count[c] / n_observations
+            clf.class_log_prior[c] = (clf.class_count[c] as f64).log2() - log_n_observations;
+            for f in 0..n_feats {
+                // pr(f | c) = feature_count[c,f] / class_count[c]
+                clf.feature_log_prob[(c,f)] = 
+                    (clf.feature_count[(c,f)] as f64).log2() - (clf.class_count[c] as f64).log2();
+            }
+        }
+        Ok(clf)
     }
 
-    fn fit(&mut self, x: DataMatrix<usize>, y: DataMatrix<usize>) {
-
+    fn predict(&self, x: DMat<usize>) -> Result<DMat<f64>, &'static str> {
+        let n_records = x.nrows();
+        let n_features = x.ncols();
+        let n_classes = self.class_count.len();
+        if n_features != self.feature_count.ncols() {
+            return Err("Data Matrix has mismatched feature count");
+        }
+        // pr(C|F) = pr(F|C) * pr(C) / pr(F)
+        // pr(C|F) = product(pr(F_i|C), i) * pr(C) / pr(F)
+        let mut y = DMat::new_zeros(n_records, n_classes);
+        for i in 0..n_records {
+            let mut divisor = 0f64;
+            for j in 0..n_classes {
+                let mut log_dividend = self.class_log_prior[j];
+                for k in 0..n_features {
+                    log_dividend += self.feature_log_prob[(j,k)];
+                }
+                y[(i, j)] = log_dividend.exp2();
+                divisor += log_dividend.exp2();
+                
+            }
+            // sum(pr(C|F), C) = 1
+            for j in 0..n_classes {
+                y[(i,j)] /= divisor;
+            }
+        }
+        Ok(y)
     }
 }
 
@@ -108,12 +184,12 @@ fn main() {
     let argv: Vec<String> = env::args().collect();
     let x_file_path = argv[1].clone();
     let y_file_path = argv[2].clone();
+    let z_file_path = argv[3].clone();
 
-    let x: DataMatrix<f32> = DataMatrix::open(&x_file_path).unwrap();
-    let y: DataMatrix<f32> = DataMatrix::open(&y_file_path).unwrap();
+    let x: DMat<usize> = DataMatrix::open(&x_file_path).unwrap().as_dmat();
+    let y: DMat<usize> = DataMatrix::open(&y_file_path).unwrap().as_dmat();
+    let z: DMat<usize> = DataMatrix::open(&z_file_path).unwrap().as_dmat();
 
-    println!("X:");
-    x.print();
-    println!("\nY:");
-    y.print();
+    let clf = MultinomialNB::fit(x, y).unwrap();
+    println!("{:?}", clf.predict(z).unwrap());
 }
